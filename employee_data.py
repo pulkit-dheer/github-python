261e2374c5689b4d1bd10996a66802e6e30a2060
Pulkit Dheer <pulkitkdheer@gmail.com>
2024-10-11 01:19:02 +0530

    employee data added

---
+++ employee_data.py
@@ -0,0 +1,12 @@
+eno = int(input("Enter Employee Number: "))
+ename = input("Enter Employee Name: ")
+esal = float(input("Enter Employee Salary: "))
+eaddr = input("Enter Employee Address: ")
+married = eval(input("Is Employee Married?[True|False]: "))
+
+print('Please confirm your provided information')
+print('Employee Number: ', eno)
+print('Employee Name: ', ename)
+print('Employee Salary: ', esal)
+print('Employee Address: ', eaddr)
+print('Employee Married: ', married)
