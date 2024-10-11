fd1480422599e229cd0abd2101caff185aec2cd5
Pulkit Dheer <pulkitkdheer@gmail.com>
2024-10-11 14:39:00 +0530

    three question on loop

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+for x in range(1,11):
+    print(f"hello who are you {x}")
+
+
+
+#Q. To display numbers from 21 to 1 in descending order:
+    for x in range(11,1,-2):
+            print(x)
+
+#Q. To print the sum of numbers in the given list:
+
+list = eval(input("Enter the list: "))
+sum = 0
+for x in list:
+      sum = sum + x 
+print(sum)
